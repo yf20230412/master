0e17bc02f62c8f1006202fbb2ff015f625f4402d
JONE <130523353+yf20230412@users.noreply.github.com>
2023-04-13 22:18:11 +0800

    Update Tiktok.js
    tiktok解锁

--- QuantumultX/js/Tiktok.js
+++ QuantumultX/js/Tiktok.js
@@ -3,12 +3,12 @@
 
 脚本功能：iPhone（免拔卡）解锁 TikTok
 下载地址：
-软件版本：
+软件版本：21.0.0
 脚本作者：Yearsf
 更新时间：2023-04-13
 
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-
+仅支持旧版解锁的重写
 *******************************
 
 [rewrite_local]
@@ -20,6 +20,6 @@
 
 [mitm] 
 
-hostname = *.tik-tokapi.com, *.byteoversea.com, *.tiktokv.com, *.,
+hostname = *.tik-tokapi.com, *.byteoversea.com, *.tiktokv.com, *.
 
 *******************************/
