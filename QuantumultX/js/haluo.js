cd909dafec22028d1181a7aedd616a1b74e6b3fd
JONE <130523353+yf20230412@users.noreply.github.com>
2023-04-28 18:52:41 +0800

    Update haluo.js
    哈啰会员

--- QuantumultX/js/haluo.js
+++ QuantumultX/js/haluo.js
@@ -18,7 +18,7 @@ hostname = a.hellobike.com
 var body=$response.body;
 body = body.replace(/"cardLevel\":\d+/g,'"cardLevel": 20');
 body = body.replace(/"cardExpireTime\":".*?\"/g,'"cardExpireTime": "2999.06.20"');
-body = body.replace(/"expireType\":\d/g,'"expireType": 1');
+body = body.replace(/"expireType\":\d/g,'"expireType": 0');
 body = body.replace(/"expireDate\":".*?\"/g,'"expireDate":"2999.06.20"');
 body = body.replace(/"cardEndTime\":".*?\"/g,'"cardEndTime":"2999-06-20T23:59:59"');
 $done(body);
