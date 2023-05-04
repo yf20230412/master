3bfc1a1bb11e8d72a003d8f52f4600b791a01cdc
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-04 15:08:55 +0800

    Update haitun1.js

--- QuantumultX/js/haitun1.js
+++ QuantumultX/js/haitun1.js
@@ -1,16 +1,16 @@
-﻿/*************************
+/*************************
 
 
 [rewrite_local]
 
-^https?:\/\/book\.haitunwallet\.com
-url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/haitun1.js
+^https:\/\/book\.haitunwallet\.com url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/haitun1.js
 
 
 [mitm]
+
 hostname = book.haitunwallet.com
 
-****************************/
+***************************/
 
 var yf=JSON.parse($response.body);
 const tt = '/app/account/members';
