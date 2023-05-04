e0285a9602553914d34a6ce031ef4811852b7106
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-04 22:47:43 +0800

    Update haluo1.js

--- QuantumultX/js/haluo1.js
+++ QuantumultX/js/haluo1.js
@@ -1,5 +1,12 @@
 
 /**********************************
+脚本名称:哈啰单车会员破解
+脚本作者: 🍭风言锋语🍎
+软件版本：
+更新时间：2023.05.03
+QQ反馈群:🌺75929916
+TG反馈: 🌺https://t.me/china_yf
+使用说明:此脚本仅供学习与交流,请勿转载与贩卖！
 [rewrite_local]
 ^https?:\/\/a\.hellobike\.com url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/haluo1.js
 
