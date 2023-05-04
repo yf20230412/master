923151cf9f54564cdb4f0933198b49d2c7a82923
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-04 13:04:15 +0800

    Add files via upload
    海豚会员破解1

---
+++ QuantumultX/js/haitun1.js
@@ -0,0 +1,35 @@
+//海豚会员破解
+/*************************
+[rewrite_local]
+
+^https?:\/\/book\.haitunwallet\.com
+url script-response-body haitun.js
+
+
+[mitm]
+hostname = book.haitunwallet.com
+
+****************************/
+
+var yf=JSON.parse($response.body);
+const tt = '/app/account/members';
+const yy = '/app/vip/status';
+const zz= '/app/user/userInfo';
+if ($request.url.indexOf(tt) != -1){
+    yf.data={
+        vipStatus = 1,
+    };
+}
+if ($request.url.indexOf(yy) != -1){
+    yf.data={
+        endTime = "2999-09-09",
+        status = 1,
+    };
+}
+    if ($request.url.indexOf(zz) != -1){
+        yf.data={
+            ctime = 88888888888
+            count = 1
+        };
+    }
+    $done({body : JSON.stringify(yf)});
