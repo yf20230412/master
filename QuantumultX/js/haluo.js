45321fe45f8626832b59f07ca1ef89fc96a278f8
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-05 00:40:51 +0800

    Update haluo.js

--- QuantumultX/js/haluo.js
+++ QuantumultX/js/haluo.js
@@ -1,11 +1,4 @@
-/* ***************************
-⚠️如果放远程，请把haluo.js替换成运程链接⚠️
-  🧚🏻‍♂️作者：🍡魔法师、木木🍡
-  wx交流群：1077223830
-🫧脚本名称:哈啰出行会员
-🫧建议配合working copy一起食用
-✈️working copy下载地址https://apps.apple.com/app/id896694807✈️
-
+/****************************
 
 [rewrite_local]
 ^https:\/\/a\.hellobike\.com\/evehicle\/api  url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/haluo.js
@@ -14,7 +7,7 @@
 [mitm]
 hostname = a.hellobike.com
 
-*************************** */
+****************************/
 var body=$response.body;
 body = body.replace(/"cardLevel\":\d+/g,'"cardLevel": 20');
 body = body.replace(/"cardExpireTime\":".*?\"/g,'"cardExpireTime": "2999.06.20"');
