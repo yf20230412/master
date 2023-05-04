93d5dd3626f2d2ffa52e43a921e57a4f1fd5da36
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-04 22:51:17 +0800

    Update haluo1.js

--- QuantumultX/js/haluo1.js
+++ QuantumultX/js/haluo1.js
@@ -19,13 +19,13 @@ var url = $request.url;
 var obj = JSON.parse(body);
 const tt = '/evehicle/api';
 if (url.indexOf(tt) != -1) {
-    obj.data.cardLevel=20,
-    obj.data.cardExpireTime="2999.06.20",
-    obj.data.expireType=0,
-    obj.data.expireDate="2999.06.20",
-    obj.data.cardEndTime="2999-06-20T23:59:59",
-    obj.data.remainingDays=9999,
-    obj.data.overdueDays=9999,
+    obj.data.cardLevel=20;
+    obj.data.cardExpireTime="2999.06.20";
+    obj.data.expireType=0;
+    obj.data.expireDate="2999.06.20";
+    obj.data.cardEndTime="2999-06-20T23:59:59";
+    obj.data.remainingDays=9999;
+    obj.data.overdueDays=9999;
     body = JSON.stringify(obj);
 }
 $done({body});
