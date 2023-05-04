e50abf484847b23889fea39900d01fb0e29ec37f
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-04 14:26:18 +0800

    Update haitun1.conf

--- QuantumultX/js/haitun1.js
+++ QuantumultX/js/haitun1.js
@@ -2,7 +2,7 @@
 /*************************
 
 
-[rewrite_remote]
+[rewrite_local]
 
 ^https?:\/\/book\.haitunwallet\.com
 url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/haitun1.js
