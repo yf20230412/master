2f2a711c9add4926c77e2de0b64c5a500a6b5908
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-04 17:40:44 +0800

    Update haitun2.js

--- QuantumultX/js/haitun2.js
+++ QuantumultX/js/haitun2.js
@@ -19,18 +19,21 @@ const zz= '/app/user/userInfo';
 if ($request.url.indexOf(tt) != -1){
     yf.data={
         "vipStatus" : 1,
+        body ：JSON.stringify(yf),
     };
 }
 if ($request.url.indexOf(yy) != -1){
     yf.data={
         "endTime" : "2999-09-09",
         "status" : 1,
+        body ：JSON.stringify(yf),
     };
 }
     if ($request.url.indexOf(zz) != -1){
         yf.data={
             "ctime" : 88888888888,
             "count" : 1,
+            body = JSON.stringify(yf),
         };
     }
     $done({body : JSON.stringify(yf)});
