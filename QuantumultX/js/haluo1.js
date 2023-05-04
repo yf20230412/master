eddea2e591c568b4a5303db471f51aaf7fc340f9
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-04 22:28:04 +0800

    Update haluo1.js

--- QuantumultX/js/haluo1.js
+++ QuantumultX/js/haluo1.js
@@ -1,4 +1,4 @@
-/**********************************
+
 脚本名称:哈啰单车会员破解
 脚本作者: 🍭风言锋语🍎
 软件版本：
@@ -7,12 +7,14 @@ QQ反馈群:🌺75929916
 TG反馈: 🌺https://t.me/china_yf
 使用说明:此脚本仅供学习与交流,请勿转载与贩卖！
 
+/**********************************
 [rewrite_local]
 ^https?:\/\/a\.hellobike\.com  url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/haluo1.js
 
 [mitm]
 hostname = a.hellobike.com
 *****************************************/
+
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
