bbc84cd2f4fb7fcc59ee1aaa536171d2f5a48cec
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-05 14:25:28 +0800

    Update haluo.js

--- QuantumultX/js/haluo.js
+++ QuantumultX/js/haluo.js
@@ -8,10 +8,12 @@
 hostname = a.hellobike.com
 
 ****************************/
-var body=$response.body;
-body = body.replace(/"cardLevel\":\d+/g,'"cardLevel": 20');
-body = body.replace(/"cardExpireTime\":".*?\"/g,'"cardExpireTime": "2999.06.20"');
-body = body.replace(/"expireType\":\d/g,'"expireType": 0');
-body = body.replace(/"expireDate\":".*?\"/g,'"expireDate":"2999.06.20"');
-body = body.replace(/"cardEndTime\":".*?\"/g,'"cardEndTime":"2999-06-20T23:59:59"');
-$done(body);
+
+var body = $response.body;
+body = body.replace(/\"cardLevel":\d+/g, '\"cardLevel":20');
+body = body.replace(/\"cardExpireTime":\.*?\/g, '\"cardExpireTime":"2999.09.09"');
+body = body.replace(/\"expireType":\d+/g, '\"expireType":1');
+body = body.replace(/\"expireDate":\.*?\/g, '\"expireDate":"2999.09.09"');
+body = body.replace(/\"cardEndTime":\.*?\/g, '\"cardEndTime":"2999-06-20T23:59:59"');
+body = body.replace(/\"oneYearCard":\w+/g, '\"oneYearCard":true');
+$done({body});
