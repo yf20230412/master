7bb763b444f10f84248d896cede70ca19eca6b7f
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-05 12:47:12 +0800

    Add files via upload
    挖财记账

---
+++ QuantumultX/js/wacaijz.js
@@ -0,0 +1,34 @@
+﻿/*******************
+ 项目名称：挖财记账
+ 下载地址：https://too.st/5ez
+ 脚本作者：风言锋语
+ 使用声明：仅供参考，转载与售卖！
+ ****************************
+ [rewrite_local]
+^https?:\/\/jz\.wacai\.com url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/wacaijz.js
+[mitm]
+hostname = jz.wacai.com
+***************************/
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+const dd = '/api/usercenter/userInfo';
+const tt = '/api/vipmember/v3';
+if (url.indexOf(dd) != -1) {
+    obj.data.isVip=1,
+    obj.data.sex=0,
+body = JSON.stringify(obj);
+}
+if (url.indexOf(tt) != -1) {
+    obj.data.adFreePermanentVip=true,
+    obj.data.adFreeVipEnable=1,
+    obj.data.expireDaysDays=99999,
+    obj.data.freeSendAdFreeVipEnable=1,
+    obj.data.freeSendVipEnable=1,
+    obj.data.isPermanentVip=true,
+    obj.data.matchVipTrial=true,
+    obj.data.vipMemberEnable=1,
+    obj.data.vipType=2,
+body = JSON.stringify(obj);    
+}
+$done({body});
