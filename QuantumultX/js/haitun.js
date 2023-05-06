fa95a74296363362e4be5b2836d5cebc6e21435d
JONE <130523353+yf20230412@users.noreply.github.com>
2023-05-06 10:49:41 +0800

    Update haitun.js

--- QuantumultX/js/haitun.js
+++ QuantumultX/js/haitun.js
@@ -21,4 +21,5 @@ body = body.replace(/"status\":\d+/g,'"status":1');
 body = body.replace(/"endTime\":"\d+-\d+-\d+"/g,'"endTime":"2999-07-10"');
 body = body.replace(/"count\":\d+/g,'"count":1');
 body = body.replace(/"ctime\":\d+/g,'"ctime":1680187630');
+body = body.replace(/"buttonTitle\":".*?\"/g,'"buttonTitle":"未完成"'
 $done(body);
