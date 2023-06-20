f7dead2b67e804539f1c59d2f5c82c27eca93579
JONE <130523353+yf20230412@users.noreply.github.com>
2023-06-20 09:19:31 +0800

    Update tiaozhuan.php

--- TvBox/tiaozhuan.php
+++ TvBox/tiaozhuan.php
@@ -2,7 +2,7 @@
  
 //重定向浏览器 
  
-header("Location: 	http://tv.nxog.top/m/111.php?ou=%E6%AC%A7%E6%AD%8C&mz=index2&xl=&jar=index2"); 
+header("Location:http://tv.nxog.top/m/111.php?ou=%E6%AC%A7%E6%AD%8C&mz=index2&xl=&jar=index2"); 
  
 //跳转至网址 
  
