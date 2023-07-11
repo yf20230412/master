e825d12f807038b76d7ceefdd7551f4f2be81ab2
JONE <130523353+yf20230412@users.noreply.github.com>
2023-07-12 02:27:01 +0800

    Update laifan.js

--- QuantumultX/js/laifan.js
+++ QuantumultX/js/laifan.js
@@ -14,21 +14,14 @@ TG反馈: 🌺https://t.me/china_yf2020
 
 
 [rewrite_local]
+^https?:\/\/lanfanapp\.com\/api\/v1 url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/lanfan.js
 
-
-^https?:\/\/lanfanapp\.com\/api\/v1 script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/laifan.js
-
-
-[mitm]
-
+[MITM]
 hostname = lanfanapp.com
 
-
-*******************************/
+*/
 var guding = $response.body;
 guding = guding.replace(/"is_purchased":\w+/g, '"is_purchased":true');
 guding = guding.replace(/"is_prime":\w+/g, '"is_prime":true');
 guding = guding.replace(/"unlocked":\w+/g, '"unlocked":true');
 $done({ body:guding});
-
-
