98e63d7ead1b4bd600c171e0db92a6fbdce68cfe
JONE <130523353+yf20230412@users.noreply.github.com>
2023-07-12 02:33:41 +0800

    Update laifan.js

--- QuantumultX/js/laifan.js
+++ QuantumultX/js/laifan.js
@@ -1,1 +1,24 @@
+/*
+[rewrite_local]
+
+
+^https?:\/\/lanfanapp\.com\/api\/v1 url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/laifan.js
+
+
+[mitm]
+
+hostname = lanfanapp.com
+
+
+*******************************/
+
+var body=$response.body;
+body = body.replace(/"is_prime":\w+/g,'"is_prime":true');
+
+body = body.replace(/"is_purchased":\w+/g, '"is_purchased":true';
+
+body = body.replace(/"unlocked":\w+/g, '"unlocked":true');
+
+$done({body});
+
 
