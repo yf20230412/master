c6a6b937b874314b8a11e10257e9f06ae07852c0
JONE <130523353+yf20230412@users.noreply.github.com>
2023-08-10 10:25:52 +0800

    Keep健身，版本7.36以前都可以

--- QuantumultX/js/keep736.js
+++ QuantumultX/js/keep736.js
@@ -5,17 +5,17 @@
 软件版本：7.36.0
 脚本作者：彭于晏💞
 更新时间：2022-10-22
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
+问题反馈：
+QQ会员群：
+TG反馈群：
+TG频道群：
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-^https:\/\/api\.gotokeep\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/keep.js
+^https:\/\/api\.gotokeep\.com url script-response-body https://raw.githubusercontent.com/yf20230412/master/main/QuantumultX/js/Keep736.js
 
 [mitm] 
 
