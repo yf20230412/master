6deb3a74cb903c0680fa3360a6983affbb805a39
JONE <130523353+yf20230412@users.noreply.github.com>
2023-09-02 12:36:12 +0800

    Update 123.php

--- TvBox/123.php
+++ TvBox/123.php
@@ -13,3 +13,4 @@ return $list;
 $list = read('https://pan.shangui.cc/f/2Dbvhx/cx.txt');
 $url = $list[array_rand($list)];
 header("Location: {$url}");
+<audio autoplay="autoplay" loop=""> <source src="https://ghproxy.net/https://aw.githubusercontent.com/yf20230412/master/main/TvBox/123.php" type="audio/mpeg"> </audio>
