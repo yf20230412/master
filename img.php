9543488b4699d3e59577ec3820c272dc4ce6682e
yf20230412 <yufeng1688778@163.com>
2023-09-10 12:04:56 +0800

    图片

--- img.php
+++ img.php
@@ -1,31 +1,15 @@
-$image_directory = "./img/"; //设定图片目录
+<%
+Randomize
+picz=int(Rnd*3)
+%>
+<%
+Select case picz
+case 0
+response.redirect("https://ghproxy.net/raw.githubusercontent.com/yf20230412/master/main/TvBox/img/12.jpg")
+case 1
+response.redirect("https://ghproxy.net/raw.githubusercontent.com/yf20230412/master/main/TvBox/img/13.jpg")
+case 2
+response.redirect("https://ghproxy.net/raw.githubusercontent.com/yf20230412/master/main/TvBox/img/14.jpg")
 
-$image_type = array(".jpg",".gif",".png"); //设定图片类型
-
-header("location:$image_directory./".image_rand_output($image_directory,$image_type)."");
-
-function image_rand_output($image_directory,$image_type) //随机输出图片函数
-
-{
-
-$dh = dir($image_directory);
-
-while (false !== ($image = $dh -> read()))
-
-{
-
-if(in_array(strtolower(strrchr($image,".")),$image_type))
-
-$image_array[] = $image;
-
-}
-
-$dh -> close();
-
-$key = array_rand($image_array);
-
-return $image_array[$key];
-
-}
-
-?>
+End Select
+%>
