ecd3f95de29664ab8428feb620b382fa7319d1de
JONE <130523353+yf20230412@users.noreply.github.com>
2023-09-10 12:38:08 +0800

    Update tiaozhuan.php

--- TvBox/tiaozhuan.php
+++ TvBox/tiaozhuan.php
@@ -1,8 +1,17 @@
+//注意：
+
+//1.location和“:”号间不能有空格，否则不会跳转。
+
+//2.在用header前不能有任何的输出。
+
+//3.header后的PHP代码还会被执行。例如，将浏览器重定向到//
+
+
 <?php
  
 //重定向浏览器 
  
-header("Location: 	https://神器每日推送.tk/pz.json"); 
+header("Location: https://www.baidu.com/"); 
  
 //跳转至网址 
  
