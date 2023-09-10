01f200f71ac2e170be7195d54580195b92eaf931
yf20230412 <yufeng1688778@163.com>
2023-09-10 12:17:07 +0800

    图片

--- img.php
+++ img.php
@@ -1,15 +1,1 @@
-<%
-Randomize
-picz=int(Rnd*3)
-%>
-<%
-Select case picz
-case 0
-response.redirect("https://ghproxy.net/raw.githubusercontent.com/yf20230412/master/main/TvBox/img/12.jpg")
-case 1
-response.redirect("https://ghproxy.net/raw.githubusercontent.com/yf20230412/master/main/TvBox/img/13.jpg")
-case 2
-response.redirect("https://ghproxy.net/raw.githubusercontent.com/yf20230412/master/main/TvBox/img/14.jpg")
-
-End Select
-%>
+```htmljavascript">window.onload=function(){window.location.href="https://ghproxy.net/raw.githubusercontent.com/yf20230412/master/main/TvBox/img/12.jpg";}```
