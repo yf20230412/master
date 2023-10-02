d6295af3126e100191edb8af1c9e0270682f6be4
yf20230412 <yufeng1688778@163.com>
2023-10-02 12:49:18 +0800

    最新102

--- TvBox/JS/music/腾讯.js
+++ TvBox/JS/music/腾讯.js
@@ -20,7 +20,7 @@ var rule = {
     timeout:5000,
     // class_parse:'.site_channel a;a&&Text;a&&href;channel/(.*)',
     cate_exclude:'会员|游戏|全部',
-    class_name:'少儿&动漫',
+    class_name:'少儿&动漫&电影&电视剧&综艺&精选&纪录片',
     class_url:'child&cartoon&movie&tv&variety&choice&doco',
     limit:20,
     // play_parse:true,
